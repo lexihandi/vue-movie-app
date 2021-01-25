14b214673b1cab8c3130f64051c2dc2fa2bd33e2
lexyhandi <lexihandi@gmail.com>
2021-01-25 22:36:59 +0700

    create feature card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,61 @@
 <template>
   <div class="home">
-    Home
+    <div class="feature-card">
+      <router-link to="/movie/tt0409591">
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/I/81vtHhR9QkL._SX450_.jpg"
+          alt="naruto poster"
+          class="featured-img"
+        />
+        <div class="detail">
+          <h3>Naruto</h3>
+          <p>
+            Naruto Uzumaki is an adolescent ninja who searches for recognition
+            and dreams of becoming the Hokage, the leader of his village.
+          </p>
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style lang="scss">
+.home {
+  .feature-card {
+    position: relative;
+
+    .feature-img {
+      display: block;
+      width: 100%;
+      width: 300px;
+      object-fit: cover;
+
+      position: relative;
+      z-index: 0;
+    }
+
+    .detail {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.6);
+      padding: 16px;
+      z-index: 1;
+
+      h3 {
+        color: #fff;
+        margin-bottom: 16px;
+      }
+
+      p {
+        color: #fff;
+      }
+    }
+  }
+}
+</style>
