094b1bde99e335ad017f95c3d09fc456dd84897c
lexyhandi <lexihandi@gmail.com>
2021-01-28 13:44:31 +0700

    update movie detail

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,10 +1,59 @@
 <template>
-  <div class="div movie-detail">Detail {{ $route.params.id }}</div>
+  <div class="div movie-detail">
+    <h2>{{ movie.Title }}</h2>
+    <p>{{ movie.Year }}</p>
+    <img :src="movie.Poster" alt="Movie Poster" class="featured-image" />
+    <p>{{ movie.Plot }}</p>
+  </div>
 </template>
 
 <script>
-export default {};
+import { ref, onBeforeMount } from "vue";
+import { useRoute } from "vue-router";
+import env from "../../env.js";
+export default {
+  setup() {
+    const movie = ref({});
+    const route = useRoute();
+
+    onBeforeMount(() => {
+      fetch(
+        `http://www.omdapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          movie.value = data;
+        });
+    });
+
+    return {
+      movie,
+      route,
+    };
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+.movie-detail {
+  padding: 16px;
+  h2 {
+    color: #fff;
+    font-size: 28px;
+    font-weight: 600;
+    margin-bottom: 16px;
+  }
+
+  .featured-image {
+    display: block;
+    max-width: 200px;
+    margin-bottom: 18px;
+  }
+
+  p {
+    color: #fff;
+    font-size: 18px;
+    line-height: 1.4;
+  }
+}
 </style>
