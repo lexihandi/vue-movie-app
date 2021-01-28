bfe06ee5be69d40f074cf9b7dd2b1720737225e9
lexyhandi <lexihandi@gmail.com>
2021-01-28 13:07:59 +0700

    update home

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,3 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
-  plugins: [
-    [
-      "module:react-native-dotenv",
-      {
-        moduleName: "@env",
-        path: ".env",
-        blacklist: null,
-        whitelist: null,
-        safe: true,
-        allowUndefined: true,
-      },
-    ],
-  ],
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,13 +26,26 @@
       <input type="submit" value="Search" />
     </form>
 
-    <div class="movies-list">Movies</div>
+    <div class="movies-list">
+      <div class="movie" v-for="movie in movies" :key="movie.imdbID">
+        <router-link :to="'/movie/' + movie.imdbID" class="movie-link">
+          <div class="product-image">
+            <img :src="movie.Poster" alt="Movie Poster" />
+            <div class="type">{{ movie.Type }}</div>
+          </div>
+          <div class="detail">
+            <p class="year">{{ movie.Year }}</p>
+            <h3>{{ movie.Title }}</h3>
+          </div>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { ref } from "vue";
-import env from "../../env";
+import env from "../../env.js";
 
 export default {
   setup() {
@@ -46,7 +59,6 @@ export default {
           .then((data) => {
             movies.value = data.Search;
             search.value = "";
-            console.log(movies.value);
           });
       }
     };
@@ -140,6 +152,63 @@ export default {
 
         &:active {
           background-color: #3b8070;
+        }
+      }
+    }
+  }
+  .movies-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0px 8px;
+
+    .movie {
+      max-width: 50%;
+      flex: 1 1 50%;
+      padding: 16px 8px;
+
+      .movie-link {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+
+        .product-image {
+          position: relative;
+          display: block;
+
+          .img {
+            display: block;
+            width: 100%;
+            height: 275px;
+            object-fit: cover;
+          }
+
+          .type {
+            position: absolute;
+            padding: 8px 16px;
+            background-color: #42b883;
+            color: #fff;
+            bottom: 16px;
+            left: 0px;
+            text-transform: capitalize;
+          }
+        }
+
+        .detail {
+          background-color: #496583;
+          padding: 15px 8px;
+          flex: 1 1 100%;
+          border-radius: 0px 0px 8px 8px;
+
+          .year {
+            color: #aaa;
+            font-size: 14px;
+          }
+
+          h3 {
+            color: #fff;
+            font-weight: 600;
+            font-size: 18px;
+          }
         }
       }
     }
